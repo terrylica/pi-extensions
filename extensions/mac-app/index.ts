27908e4def44b8b52edc251c8a684fb79fdc4d1b
Aliou Diallo <code@aliou.me>
2026-02-07 15:33:02 +0100

    chore: remove mac-app extension

--- extensions/mac-app/index.ts
+++
@@ -1,6 +0,0 @@
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-import { setupMacAppTools } from "./tools";
-
-export default function (pi: ExtensionAPI) {
-  setupMacAppTools(pi);
-}
