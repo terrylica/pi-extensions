40c908dcfc57512c7e3167245f299afbfe17e7e5
Aliou Diallo <code@aliou.me>
2026-02-17 16:46:47 +0100

    test: setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    include: ["extensions/**/*.test.ts", "packages/**/*.test.ts"],
+    mockReset: true,
+  },
+});
