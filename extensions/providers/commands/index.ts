e8db85add4dac6d3973628bd34e3eda8b8f389d5
Aliou Diallo <code@aliou.me>
2026-03-14 22:26:30 +0100

    refactor(providers): remove toggle widget command

--- extensions/providers/commands/index.ts
+++ extensions/providers/commands/index.ts
@@ -1,10 +1,8 @@
 import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
 import { registerProvidersSettings } from "./settings-command";
-import { setupToggleBarCommand } from "./toggle-widget";
 import { setupUsageCommand } from "./usage";
 
 export function setupUsageCommands(pi: ExtensionAPI): void {
   setupUsageCommand(pi);
-  setupToggleBarCommand(pi);
   registerProvidersSettings(pi);
 }
