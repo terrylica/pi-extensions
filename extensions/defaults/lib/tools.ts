e7b4a66273aab8afabbdbd16db9c792fcc113dba
Aliou Diallo <code@aliou.me>
2026-04-01 11:34:47 +0200

    fix: remove custom edit tool override, use native prepareArguments

--- extensions/defaults/lib/tools.ts
+++ extensions/defaults/lib/tools.ts
@@ -1,6 +1,5 @@
 import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
 import { setupBashTool } from "../tools/bash";
-import { setupEditTool } from "../tools/edit";
 import { setupFindTool } from "../tools/find";
 import { setupGetCurrentTimeTool } from "../tools/get-current-time";
 import { setupReadTool } from "../tools/read";
@@ -8,7 +7,6 @@ import { setupReadUrlTool } from "../tools/read-url";
 
 export function setupTools(pi: ExtensionAPI): void {
   setupReadTool(pi);
-  setupEditTool(pi);
   setupFindTool(pi);
   setupBashTool(pi);
   setupGetCurrentTimeTool(pi);
