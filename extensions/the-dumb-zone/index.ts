48a4366e5f6a65dfeddeac4fac9418f8c4814ebf
Aliou Diallo <code@aliou.me>
2026-04-01 15:02:15 +0200

    chore: add placeholder for moved the-dumb-zone extension

---
+++ extensions/the-dumb-zone/index.ts
@@ -0,0 +1,7 @@
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+
+export default function (_pi: ExtensionAPI) {
+  console.error(
+    "[the-dumb-zone] This extension has moved to https://github.com/aliou/pi-undercooked",
+  );
+}
